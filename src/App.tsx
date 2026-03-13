0f2e3a1b76ae7e1a8e87ac330b3a1cd252eb41ee
stewcio <dawid.moskalik@cyfroweszkoly.pl>
2026-03-13 10:56:03 +0100

    remove kalkulator.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
+/*import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/vite.svg'*/
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <div>
+              <form method="POST" action="">
+                  <label><input type="text" id="imie"/></label>
+                  <label>pojazdy</label>
+                  <select id="sprzet" name="sprzet">
+                      <option value="nic">wybierz pojazd</option>
+                      <option value="Kajak">Kajak</option>
+                      <option value="Rower wodny">Rower wodny</option>
+                      <option value="Omega">Omega</option>
+                  </select>
+                  <input type="range"/>
+
+                  <label>kapok<input type="checkbox"/></label>
+                  <label>instruktor<input type="checkbox"/></label>
+              </form>
+          </div>
+      </>
   )
 }
 
